584454ae3185b2a32f36c761cbce5c799d8ea168
Claudio d'Angelis <claudiodangelis@gmail.com>
2020-03-31 19:46:51 +0200

    install script

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@
 # Ensure you are in the right place
 if ! grep -q 'claudiodangelis/dotfiles' .git/config > /dev/null 2>&1; then
     echo "This script must be run from the claudiodangelis/dotfiles repository"
+    exit 1
 fi
 # Take a backup of the files you are going to work with
 BACKUPDIR=backups/$(date +%s)
 mkdir -p $BACKUPDIR
 cp ~/.bashrc ~/.dotfiles ~/.env ~/.aliases $BACKUPDIR
-
+echo "- backup taken: $BACKUPDIR"
 # Install dependencies and tools
 # z.sh
 # hstr
